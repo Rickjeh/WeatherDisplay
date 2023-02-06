e787015ced1026856ff260554ba9d81d2b74ddcd
Rickjeh <info@rickjeh.dev>
2023-02-06 13:55:53 +0100

    1.18.2 commit

---
+++ src/main/java/dev/rickjeh/weatherdisplay/Weatherdisplay.java
@@ -0,0 +1,10 @@
+package dev.rickjeh.weatherdisplay;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Weatherdisplay implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
